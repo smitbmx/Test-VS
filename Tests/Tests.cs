506e0a890493c72f94caadc1e729f8528f912c63
smitbmx <smitbmx@gmail.com>
2012-06-30 17:01:23 +0300

    version with tests

---
+++ Tests/Tests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
